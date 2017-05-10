a142c90356ff28c34618cc2ba242fb103e4bd601
Giraffpaj <jacob.thorstenson@gmail.com>
2017-05-10 10:51:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <SFML/Graphics.hpp>
 #include <ctime>
 #include "MapGenerator.h"
+#include "GameLogic.h"
 
 int main()
 {
+	GameLogic myGameLogic;
+	//data skapats
 	srand(static_cast<unsigned>(time(0)));
 	//sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
+	
+	//looop
+	
+	
+	
+	
+	myGameLogic->tick();
+	
 	MapGenerator mG(32,25);
 	mG.createMapDesign();
 	mG.draw();
+	
+	//slut ppå loop
 }
