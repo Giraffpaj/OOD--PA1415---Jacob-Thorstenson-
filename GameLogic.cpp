ae77ca37eb79f315cd161390d46abe0cbb8a7f53
Giraffpaj <jacob.thorstenson@gmail.com>
2017-05-15 15:44:35 +0200

    Update GameLogic.cpp

--- GameLogic.cpp
+++ GameLogic.cpp
@@ -1,8 +1,18 @@
 #include "GameLogic.h"
 
+void GameLogic::clearAll()
+{
+	delete this->myButtonListener;
+	delete this->myDrawHandler;
+	delete this->myMovementHandler;
+	delete this->myEvents;
+	delete this->myLibrary;
+	delete this->myMapGenerator;
+}
+
 GameLogic::GameLogic()
 {
-	this->myLibrary = new Library();  //Denna måste skapas först för den skall skickas till de flesta handlers!
+	this->myLibrary = new Library();  //Denna mÃ¥ste skapas fÃ¶rst fÃ¶r den skall skickas till de flesta handlers!
 	this->myEvents = new ArrayList<Event>(20);
 	this->myButtonListener = new ButtonListener();
 	this->myMovementHandler = new MovementHandler(this->myLibrary);
@@ -12,14 +22,14 @@ GameLogic::GameLogic()
 	this->myMapGenerator->createExitTile();
 	this->myMapGenerator->drawMap();
 	
-	//Ful-lösning
+	//Ful-lÃ¶sning
 	string*dataTxt = nullptr;
 	int id = 1;
 	string handler = "DrawHandler";
 	string action = "addObjectByID";
 	Event*addPlayer = new Event("?", id, handler, action);
 	this->myEvents->add(addPlayer);
-	//slut på Ful-lösning
+	//slut pÃ¥ Ful-lÃ¶sning
 }
 
 void GameLogic::addEvent(Event*myEvent)
@@ -31,7 +41,7 @@ void GameLogic::tick(sf::RenderWindow &myRenderWindow)
 {
 	Event*currentEvent;
 	this->addEvent(this->myButtonListener->checkPressedButtons());
-	for(int a = 0;a<this->myEvents->getSize()+1;a++) //lägger till +1 för att det finns ett extra event som ligger över från initierings process
+	for(int a = 0;a<this->myEvents->getSize()+1;a++) //lÃ¤gger till +1 fÃ¶r att det finns ett extra event som ligger Ã¶ver frÃ¥n initierings process
 	{
 		currentEvent = this->myEvents->getData(a);
 
@@ -59,9 +69,9 @@ void GameLogic::tick(sf::RenderWindow &myRenderWindow)
 			this->myDrawHandler->addEvent(currentEvent);
 			//skicka event till MapHandler
 		}
-		this->myEvents->remove(currentEvent);  //Tar bort event från listan
+		this->myEvents->remove(currentEvent);  //Tar bort event frÃ¥n listan
 	}
 	this->myMovementHandler->tick();
 	this->myDrawHandler->tick();
-	this->myDrawHandler->draw(myRenderWindow); //kan även läggas in i slutet på tick i DrawHandler
+	this->myDrawHandler->draw(myRenderWindow); //kan Ã¤ven lÃ¤ggas in i slutet pÃ¥ tick i DrawHandler
 }
